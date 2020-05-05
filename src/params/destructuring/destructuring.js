6a7166dad24501fd03758328688d34261e35eb56
Jeongmu.Park <jeongmupark@gmail.com>
2020-05-05 23:22:40 +0900

    해체 할당 ing

---
+++ src/params/destructuring/destructuring.js
@@ -0,0 +1,9 @@
+const landscape = {
+  title: "Landscape",
+  photographer: "Nathan",
+  equipment: "Canon",
+  format: "digital",
+  src: "/landscape-nm.jpg",
+  location: [32.7122222, -103.1405556]
+};
+
