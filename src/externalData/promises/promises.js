04d86a7a6af2078570d51d0ce9abf94eb2184be4
Jeongmu.Park <jeongmupark@gmail.com>
2020-05-07 00:43:28 +0900

    promise

--- src/externalData/promises/promises.js
+++ src/externalData/promises/promises.js
@@ -1,7 +1,35 @@
-function geMusic(theme) {
+function getUserPreferences() {
+  const preferences = new Promise((resolve, reject) => {
+    resolve({
+      theme: "duskError"
+    });
+  });
+  return preferences;
+}
+
+function getMusic(theme) {
   if (theme === "dusk") {
     return Promise.resolve({
       album: "music for airports"
     });
   }
+  return Promise.reject({
+    album: "kind of blue"
+  });
 }
+
+function getArtist(album) {
+  return Promise.resolve({
+    artist: "Brian Eno"
+  });
+}
+
+getUserPreferences()
+  .then(preference => getMusic(preference.theme))
+  .then(music => getArtist(music.album))
+  .then(artist => {
+    console.log(artist.artist);
+  })
+  .catch(e => {
+    console.log('error' + e);
+  });
